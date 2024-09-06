15389f32d988cf9f8bd3079b3474b8666783ea9f
yuvraj08-netweb <yuvraj.netweb@gmail.com>
2024-09-06 16:31:41 +0530

    project02 completed

--- project02/src/App.jsx
+++ project02/src/App.jsx
@@ -1,12 +1,44 @@
-import './App.css'
-
+import { useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./contexts/Theme";
+import { useEffect } from "react";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card"
 function App() {
+  const [themeMode,setThemeMode] = useState("light");
+
+  const lightTheme = () =>{
+      setThemeMode("light");
+  }
+
+  const darkTheme = () =>{
+      setThemeMode("dark");
+  }
+
+  //Actual Change in theme
+
+  useEffect(()=>{
+    document.querySelector("html").classList.remove("light","dark")
+    document.querySelector("html").classList.add(themeMode)
+  },[themeMode])
 
   return (
-    <>
-     
-    </>
-  )
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* Theme Button */}
+        <ThemeBtn/>
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* CARD */}
+            <Card/>
+          </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
